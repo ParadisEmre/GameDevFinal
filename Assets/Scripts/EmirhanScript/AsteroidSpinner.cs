6d8b77c52ebb72323a37856888c48b9e8880ecbb
Emirhan Akıtürk <emirhanakiturk@gmail.com>
2023-06-11 14:01:31 +0300

    AsteroidSpinner

---
+++ Assets/Scripts/EmirhanScript/AsteroidSpinner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpinner : MonoBehaviour
+{
+    private float speed = 1f;
+    private float x, y, z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = Random.Range(-speed, speed);
+        y = Random.Range(-speed, speed);
+        z = Random.Range(-speed, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Space.self rotates the object relative to its own axis 
+        transform.Rotate(x, y, z, Space.Self);
+    }
+}
+
+
